8a0bda271dcf7b099c7860fd30451a372c2736ca
Matthew Goldsmith <31556412+mgoldsmith1@users.noreply.github.com>
2018-08-21 20:32:23 -0400

    Update sample1_unittest.cc

--- src/sample1_unittest.cc
+++ src/sample1_unittest.cc
@@ -9,22 +9,7 @@ namespace {
 
   // Tests Binary Tree for Max Distinct Path Value
   TEST(BinartTreeTest, Trivial) {
-    
     EXPECT_EQ(4, solution());
-    
   }
-
+  
 }
-
-
-// TO DO:
-// isEmpty() 
-
-// Larger Data Sets
-
- //EXPECT_FALSE((1) == (solution()));
- //EXPECT_FALSE((2) == (solution()));
- //EXPECT_FALSE((3) == (solution()));
-
- //EXPECT_FALSE((5) == (solution()));
-
